3f379314aaa7cefe005b14cbb3946703b60cb459
Tim Jay <tim.joyce33@gmail.com>
2018-05-01 17:37:56 -0400

    Added block to store info
    Site is not returning headers for our useragent

--- youtube_dl/extractor/tirerack.py
+++ youtube_dl/extractor/tirerack.py
@@ -7,8 +7,9 @@ from .common import InfoExtractor
 class TireRackIE(InfoExtractor):
     #_VALID_URL = r'https?://(?:www\.)?tirerack\.com/watch/(?P<id>[0-9]+)'
     #_VALID_URL = r'https?://(?:www\.)?tirerack\.com/videos/(?P<id>[0-9]+)'
-    _VALID_URL = r'https?://(?:www\.)?videos\.?tirerack\.com/.*'
-
+    _VALID_URL = r'https?://(?:www\.)?videos\.?tirerack\.com/videos/'
+    
+    
 
     _TEST = {
         'url': 'https://www.tirerack.com/videos/index.jsp?video=545',
@@ -26,23 +27,44 @@ class TireRackIE(InfoExtractor):
         }
     }
 
+    def _extract_video(self, source):
+        return {
+            'id': source.get('videoId'),
+            'title': source.get('title'),
+            'description': source.get('description'),
+            'thumbnail': source.get('highResImage') or source.get('medResImage'),
+            'uploader': source.get('username'),
+            'duration': int_or_none(source.get('length')),
+            'view_count': int_or_none(source.get('views')),
+            'age_limit': 18 if source.get('isMature') == 'true' or source.get('isSexy') == 'true' else 0,
+            'formats': self._extract_formats(source),
+        }
+            
     def _real_extract(self, url):
-        video_id = self._generic_id(url)
-        webpage = self._download_webpage(url, video_id)
+        #header = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+        mobj = re.match(self._VALID_URL, url)
+        video_id = mobj.group('id')
 
+        site_id = self._match_id(url)
+        webpage = self._download_webpage(url, site_id)
+
+
+     
         # TODO more code goes here, for example ...
-        title = self._og_search_title(webpage)
-        description = self._og_search_description(webpage)
+        #title = self._og_search_title(webpage)
+        #description = self._og_search_description(webpage)
 
         # example mp4 link
         # https://videos.tirerack.com/pdl/aa/73/aa732da5-5774-4abf-9d73-554121d0df29/03_Tire_Decision_Guide_30_800k.mp4
-        video_url = self._html_search_meta(r'https://videos.tirerack.com/pdl/.*')
+        #video_url = self._html_search_meta(r'https://videos.tirerack.com/pdl/.*')
 
         return {
             'id': video_id,
-            'url': video_url,
-            'title': title,
-            'description': description,
+            'url': site_id,
+            #'url': 'https://videos.tirerack.com/pdl/aa/73/aa732da5-5774-4abf-9d73-554121d0df29/03_Tire_Decision_Guide_30_800k.mp4',
+            'title': 'title',
+            'description': 'description',
+            'header': {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
             #'uploader': self._search_regex(r'<div[^>]+id="uploader"[^>]*>([^<]+)<', webpage, 'uploader', fatal=False),
             # TODO more properties (see youtube_dl/extractor/common.py)
         }
